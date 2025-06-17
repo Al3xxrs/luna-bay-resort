4e28c4a4d87f9e335ec260f21a6dcf80ce053309
alex <al3xx.main1@gmail.com>
2025-06-17 12:25:47 +0200

    fix mismatched data

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use("/api/contact", contactRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Server listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 18473;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
